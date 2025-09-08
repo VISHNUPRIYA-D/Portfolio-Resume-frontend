eab6cba41bbe173d018a453fc28cb3f9b203cec2
vishnupriya <vishnupriyad317@gmail.com>
2025-09-08 12:05:49 +0530

    template design changes

--- src/pages/Template.jsx
+++ src/pages/Template.jsx
@@ -2,12 +2,17 @@ import React, { useContext } from "react";
 import { useParams } from "react-router-dom";
 import { PortfolioContext } from "../context/PortfolioContext";
 import { FaArrowLeft } from "react-icons/fa6";
-import ExportPortfolio from "../components/ExportPortfolio";
 
 const Template = () => {
   const { id } = useParams();
-  const { templates, finalUser, def, handleDownloadPdf, printRef, navigate, saveDownload,setSaveDownload } =
-    useContext(PortfolioContext);
+  const {
+    templates,
+    finalUser,
+    def,
+    handleDownloadPdf,
+    printRef,
+    navigate,
+  } = useContext(PortfolioContext);
 
   // find template by id
   const selectedTemplate = templates.find((t) => t.id.toString() === id);
@@ -20,6 +25,7 @@ const Template = () => {
 
   return (
     <div className="min-h-screen dark:bg-gray-800 bg-gray-100 dark:text-white px-4 sm:px-6 py-6 flex flex-col">
+      {/* Header */}
       <div className="flex items-center gap-3 sm:gap-5 mb-6">
         <FaArrowLeft
           onClick={() => navigate("/")}
@@ -30,21 +36,22 @@ const Template = () => {
         </h2>
       </div>
 
+      {/* Template Preview */}
       <div
         ref={printRef}
-        className="bg-white rounded-xl shadow-md max-w-5xl mx-auto  sm:p-6 lg:p-10 overflow-x-auto"
+        className="bg-white rounded-xl shadow-md w-full max-w-7xl mx-auto sm:p-6 lg:p-10 overflow-x-auto"
       >
         <Component user={finalUser} def={def} />
       </div>
 
+      {/* Download Button */}
       <div className="flex justify-center sm:justify-end">
-       <button
-          onClick={()=>handleDownloadPdf(selectedTemplate.id)}
+        <button
+          onClick={() => handleDownloadPdf(selectedTemplate.id)}
           className="mt-6 bg-blue-500 hover:bg-blue-600 text-white text-sm sm:text-lg px-4 sm:px-6 py-2 rounded-lg shadow-md transition-all"
         >
           Download <span className="hidden sm:inline">as PDF</span>
         </button>
-       
       </div>
     </div>
   );
